997f428dd04afbb2ff17e5185122e09eee4e3de0
Serge Rodovnichenko <sergerod@gmail.com>
2020-12-08 11:31:19 +0300

    Autogenerated

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Размеры для веса',
+  'img' => 'img/sizer.gif',
+  'version' => '1.0.0',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopSizerPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopSizerPlugin extends shopPlugin
+{
+
+}
