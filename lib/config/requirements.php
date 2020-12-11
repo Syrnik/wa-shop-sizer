46370e1f7a2986e9e37a00286782989c0747f332
Serge Rodovnichenko <sergerod@gmail.com>
2020-12-11 04:21:41 +0300

    Иконки, требования к системе и т.д.

---
+++ lib/config/requirements.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @copyright Serge Rodovnichenko, 2020
+ */
+return [
+    'php'      => ['version' => '>=7.2.0', 'strict' => true],
+    'app.shop' => ['version' => '>=8.0.0', 'strict' => true]
+];
