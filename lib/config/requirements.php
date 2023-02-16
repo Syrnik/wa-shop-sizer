3cd12791ad9b7561e8acea2c7d04e88252c4cd0c
Serge Rodovnichenko <sergerod@gmail.com>
2023-02-16 21:20:45 +0300

    fix codestyle

--- lib/config/requirements.php
+++ lib/config/requirements.php
@@ -3,6 +3,7 @@
  * @author Serge Rodovnichenko <serge@syrnik.com>
  * @copyright Serge Rodovnichenko, 2020-2023
  */
+
 return [
     'php'      => ['version' => '>=8.0.0', 'strict' => true],
     'app.shop' => ['version' => '>=8.0.0', 'strict' => true]
