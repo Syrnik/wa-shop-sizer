c27cd9d6115280897f75af61c4521b647b47b6af
Serge Rodovnichenko <sergerod@gmail.com>
2023-02-16 21:31:58 +0300

    Version up

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -2,7 +2,7 @@
 return array(
     'name'     => 'Размеры для веса',
     'img'      => 'img/icon.png',
-    'version'  => '1.0.0',
+    'version'  => '2.0.0',
     'vendor'   => '670917',
     'handlers' => ['shipping_package' => 'handlerShippingPackage'],
 );
